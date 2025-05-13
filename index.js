47112832e50324a0c702944e24d672a8d84fabeb
Aryan Dadhich <Lovelydadhich143@gmail.com>
2025-05-13 19:46:49 +0530

    ejs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const urlroute = require('./routes/url')
+const path = require("path");
 const {connectToMongodb} = require('./connect')
 const app = express();
 const Port = 8001;
@@ -9,6 +10,9 @@ connectToMongodb('mongodb://127.0.0.1:27017/short-url')
 .then(()=> {
   console.log("connected to mongodb")
 })
+
+app.set("view engine", "ejs");
+app.set("views",path.resolve('./views'));
 
 app.use(express.json()) // this is used to parse the json data from the body of the request
 app.use("/url", urlroute);
