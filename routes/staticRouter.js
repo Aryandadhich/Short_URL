65de9eecbeced3b2661dee1d02ae4d91e8ed9d92
Aryan Dadhich <Lovelydadhich143@gmail.com>
2025-05-20 18:04:53 +0530

    table view

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -1,9 +1,17 @@
-const express = require ("express");
+const express = require ("express"); // importing epxress its used to create a server and define routes
+const URL = require('../models/url'); // importing the URL model to interact with the database
 
-const router = express.Router();
+const router = express.Router(); 
 
-router.get('/', (req,res)=>{
-    return res.render("home")
+router.get('/',  async (req,res)=>{ 
+    const allurls = await URL.find({})
+    return res.render("home"), {
+        urls: allurls
+    }
 })
+// async function to handle the request and response and 
+// let you use await inside the function
+// and await paused the code execution until the async task is completed
+// and then it will continue to execute the code
 
 module.exports = router;
