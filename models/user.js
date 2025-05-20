10c904394d6e2da31afe7a84032dca3b11f9379f
Aryan Dadhich <Lovelydadhich143@gmail.com>
2025-05-20 23:26:00 +0530

    Autjentication

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose'); //moongose a lib used to inetract with mongodb using model and schema
+const userSchema = new mongoose.Schema({  // schema is a blueprint for the data we are creating one for user collection
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+},{timestamps:true});
+
+const User = moongose.model('user',userSchema);
+
+module.export = User;
